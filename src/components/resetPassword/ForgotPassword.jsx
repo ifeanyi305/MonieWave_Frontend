ed5e0ea706d65a0d16e7fcacb912ce85e039138a
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-05-12 21:06:20 +0100

    start styling

--- src/components/resetPassword/ForgotPassword.jsx
+++ src/components/resetPassword/ForgotPassword.jsx
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom';
 const ForgotPassword = () => {
   return (
     <div>
-      <p>you Forget your password oya change am</p>
-      <Link to='/ResetPassword'>ResetPassword</Link>
+      <div className="flex flex-col justify-center">
+        <h1 className="pb-[15px]">Forget your password?</h1>
+        <p>If you are logged out of your RathHive account and can't remember your password</p>
+        <p>we can send you an email with a link to reset your password</p>
+        <Link to='/ResetPassword'>ResetPassword</Link>
+      </div>
     </div>
   );
 };
